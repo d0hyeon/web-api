7db6bd33da861f8c63ffd02ded6f86e8e31ac398
d0hyeon <ehgus6887@gmail.com>
2021-01-25 18:32:20 +0900

    스타일 모듈 분리

---
+++ src/components/styles/input.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+
+export interface InputProps {
+  full?: boolean;
+  width?: number;
+  height?: number; 
+}
+
+export const SInput = styled.input<InputProps>`
+  border: 1px solid #ddd;
+  padding: 0 10px;
+  line-height: 28px;
+
+  ${({width, height, full}) => `
+    width: ${full ? '100%' : (width ? `${width}px` : 'auto')}
+    height: ${height ?? 30}px;
+  `}
+`
