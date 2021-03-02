49b8087534772d3978786d45d5fab00b9a316c71
d0hyeon <ehgus6887@gmail.com>
2021-03-03 03:47:22 +0900

    feat: Highlight 컴포넌트 lazy loading 적용
     - 내용 수정

--- src/pages/Performance/Observer.tsx
+++ src/pages/Performance/Observer.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
 import { P, Code, H1, H2, H3 } from '@src/components/styles/text';
 import { Header, Section, Ul } from '@src/components/styles/common';
-import Highlight from 'react-highlight';
 import Toggle from '@src/components/Toggle';
+import AsyncToggle from '@src/components/AsyncToggle';
 import PerformanceTestButton from '@src/components/PerformanceTestButton';
+
 
 interface RecordPerformance {
   name: string;
@@ -34,6 +34,42 @@ const Ovserver: React.FC = () => {
       }
     })
   }, [setRecords]);
+  
+  const Highlight = React.useMemo(() => {
+    return React.lazy(() => import('react-highlight'));
+  }, []);
+
+  const images = (process.env.REACT_APP_DUMP_IMAGES as string).split(',');
+
+  const getContents = React.useCallback((open) => {
+    const content = (
+      <>
+        {images.map((url, idx) => (
+          <p key={`${url}-${idx}`}>
+            <img src={url} width="100" alt="person"/>
+          </p>
+        ))}
+        <React.Suspense fallback={null}>
+          {/* @ts-ignore */}
+          <Highlight>
+            <div>{`
+      observer.observe({entryTypes: ['mark', 'measure']});
+      const records = observer.takeRecords();
+      console.log(records[0]);
+              `.trim()}
+            </div>
+          </Highlight>
+        </React.Suspense>
+    </>
+    )
+    if(open) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(content);
+        }, 500)
+      })
+    }
+  }, [images, Highlight]);
 
   React.useEffect(() => {
     observer.observe({entryTypes: ['mark', 'measure']});
@@ -48,17 +84,27 @@ const Ovserver: React.FC = () => {
           <H1>PerformanceObserver</H1>
           <P>PerfomanceObserver는 성능 측정 이벤트를 관찰하고 브라우저의 성능 타임라인에 기록 되는 항목에 대한 알림을 받는다.</P>
           <Toggle title={<P>예제</P>}>
-            <Highlight>
-                {`
+            <React.Suspense fallback="">
+              <>  
+                {/* @ts-ignore */}
+                <Highlight>
+{`
 const observer = new PerformanceObserver(list => {
-  const entries = list.getEntries();
-  
+const entries = list.getEntries();
+    
   for(let i = 0, length = entries; i < length; i ++) {
     console.log(entries[i]);
   }
-})`.trim()
-              }
-              </Highlight>
+})`.trim()}
+                </Highlight>
+                <img src={images[0]} alt="person"/>
+                {images.map((url) => (
+                  <p key={url}>
+                    <img src={url} width="100" alt="person"/>
+                  </p>
+                ))}
+              </>
+            </React.Suspense>
           </Toggle>
         </Header>
         <Section>
@@ -68,24 +114,33 @@ const observer = new PerformanceObserver(list => {
               <P>observe</P>
               <P>성능 측정 할 대상을 등록한다. 옵션으로 <Code>entryTypes</Code> <Code>type</Code> <Code>buffered</Code> 을 작성 해줄수 있다.</P>
               <Toggle title={<P>예제</P>}>
-                <Highlight>
-                {`
-                  observer.observe({entryTypes: ['mark', 'frame', 'measure']});
-                `.trim()}
-                </Highlight>
+                <React.Suspense fallback="">
+                  {/* @ts-ignore */}
+                  <Highlight>
+                  {`
+                    observer.observe({entryTypes: ['mark', 'frame', 'measure']});
+                  `.trim()}
+                  </Highlight>
+                </React.Suspense>
               </Toggle>
               </li>
               <li>
                 <P>takeRecoreds</P>
                 <P>측정 된 값을 기록한다</P>
+                <AsyncToggle title={<P>예제</P>} >
+                  {getContents}
+                </AsyncToggle>
                 <Toggle title={<P>예제</P>}>
-                  <Highlight>
-                    {`
-observer.observe({entryTypes: ['mark', 'measure']});
-const records = observer.takeRecords();
-console.log(records[0]);
-                    `.trim()}
-                  </Highlight>
+                  <React.Suspense fallback="">
+                    {/* @ts-ignore */}
+                    <Highlight>
+                      {`
+  observer.observe({entryTypes: ['mark', 'measure']});
+  const records = observer.takeRecords();
+  console.log(records[0]);
+                      `.trim()}
+                    </Highlight>
+                    </React.Suspense>
                 </Toggle>
             </li>
           </Ul>
@@ -113,8 +168,8 @@ console.log(records[0]);
           <H3>해당 페이지의 로딩속도저하 원인</H3>
           <Ul>
             <li>1. font 파일 로딩 </li>
-            <li>2. highlight 모듈 로딩 - 호출과 사용의 비용이 상당함.</li>
-            <li>3. 토글 컴포넌트 내의 highlight 마운트 (닫혀있는데 불필요한 마운트)</li>
+            <li><del>2. highlight 모듈 로딩 - 호출과 사용의 비용이 상당함.</del> - lazy loading 적용</li>
+            <li><del>3. 토글 컴포넌트 내의 highlight 마운트 (닫혀있는데 불필요한 마운트)</del> - Toggle 컴포넌트 전면 개편</li>
           </Ul>
         </Section>
     </>
