8d31d772280672ce2acb6d958505a82c87332f3f
d0hyeon <ehgus6887@gmail.com>
2021-03-03 03:01:27 +0900

    feat: toggle 컴포넌트 개편
     - isOpen일 때 컨텐츠 언마운트 ( 닫혀있을 때 불필요한 컴포넌트 로딩 제거 )
    - 미디어 엘리먼트 all 로드 시 사이즈 계산
    
     외 로직 및 성능 개선

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,60 +1,104 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import ResizeObservable from '@src/lib/ResizeObservable';
+import { onLoadChildren } from '@src/utils/element';
 
-interface Props {
+export interface ToggleProps {
   title: React.ReactNode;
   children: React.ReactNode;
+  duration?: number;
 }
 
-const Toggle: React.FC<Props> = ({children, title}) => {
+const ob = new ResizeObservable();
+
+const Toggle: React.FC<ToggleProps> = ({title, children, duration = 300}) => {
   const contentRef = React.useRef<HTMLDivElement>(null);
   const lastHeightRef = React.useRef<number>(0);
   const [heightValue, setHeightValue] = React.useState<string>('0px');
-  const [isInit, setIsInit] = React.useState<boolean>(false);
+  const [isInit, setIsInit] = React.useState<boolean>(false); // 실 컨텐츠 높이가 구해졌는가?
   const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const [isAnimating, setIsAnimating] = React.useState<boolean>(false);
 
-  const ob = React.useMemo(() => new ResizeObservable(), []);
   const resizeHandler = React.useCallback(({height}: DOMRect) => {
-    if((isOpen || !isInit) && height !== lastHeightRef.current) {
+    if(isOpen && height) {
       lastHeightRef.current = height;
-      setIsInit(true);
     }
-  }, [isOpen, isInit, setIsInit, lastHeightRef]);
+  }, [isOpen, lastHeightRef]);
 
   React.useLayoutEffect(() => {
     ob.register(contentRef.current as HTMLDivElement, resizeHandler);
     return () => ob.disconnect();
-  }, [ob, resizeHandler, contentRef]);
+  }, [resizeHandler, contentRef]);
 
   React.useLayoutEffect(() => {
+    const transitionStartHandler = () => setIsAnimating(true);
+    const transitionCancleHandler = () => setIsAnimating(false);
     // 내용이 열리고 동적으로 컨텐츠가 추가 됬을 때 사이즈 감지를 위해 스타일 속성 중 height 를 auto로 변환시킴.
-    const transitionEndHandler = () => setHeightValue('auto');
+    const transitionEndHandler = () => {
+      setIsAnimating(false);
+      setHeightValue(curr => {
+        return curr === '0px' 
+          ? curr // 닫는 시점에서는 auto가 될 필요 없음
+          : 'auto'
+      });
+    };
+    contentRef.current?.addEventListener('transitionstart', transitionStartHandler);
     contentRef.current?.addEventListener('transitionend', transitionEndHandler);
+    contentRef.current?.addEventListener('transitioncancel', transitionCancleHandler);
 
-    return () => contentRef.current?.removeEventListener('transitionend', transitionEndHandler);
-  }, [contentRef, setHeightValue]);
+    return () => {
+      contentRef.current?.removeEventListener('transitionstart', transitionStartHandler);
+      contentRef.current?.removeEventListener('transitionend', transitionEndHandler);
+      contentRef.current?.removeEventListener('transitioncancel', transitionCancleHandler);
+    }
+  }, [contentRef, setHeightValue, setIsAnimating]);
 
-  const toggleHandler = React.useCallback(() => {
+  const toggleHandler = React.useCallback<React.MouseEventHandler<Element>>(() => {
+    if(isAnimating) {
+      return;
+    }
     // 열고 닫을 때만 스타일에 높이값을 직접 부여하여 transition 효과를 줌
-    setHeightValue(lastHeightRef.current + 'px');
-    setTimeout(() => {
-      setIsOpen(curr => !curr);
-    }, 1)
-  }, [setIsOpen, lastHeightRef]);
+    if(isOpen) {
+      // 닫을때
+      setHeightValue(lastHeightRef.current + 'px');
+      setTimeout(() => {
+        setHeightValue('0px');
+        setTimeout(() => {
+          setIsOpen(false); 
+        }, duration);
+      });
+    } else {
+      // 열때
+      setIsOpen(true);
+    }
+  }, [isOpen, isAnimating, setIsOpen, setIsInit, lastHeightRef, duration]);
+  
+  React.useEffect(() => {
+    if(isOpen) {
+      onLoadChildren((contentRef.current) as Element, 'img')
+        .then(() => {
+          setIsInit(false);
+          setTimeout(() => {
+            setIsInit(true);
+            setHeightValue('0px');
+            setTimeout(() => setHeightValue(lastHeightRef.current + 'px'), 20);
+          }, 30)
+        })
+    }
+  }, [isOpen, setHeightValue, setIsInit, lastHeightRef, contentRef])
 
-  return ( 
+  return (
     <Wrapper className="toggle-article">
-      <Title isOpen={isOpen} onClick={toggleHandler} className="toggle__title">{title}</Title>
+      <Title isOpen={heightValue !== '0px' && isOpen} onClick={toggleHandler} duration={duration} className="toggle__title">{title}</Title>
       <Content 
-        isOpen={isOpen}
         height={heightValue}
         isInit={isInit} 
         ref={contentRef} 
+        duration={duration}
         will-change="true"
         className={`toggle__content ${isOpen ? '' : 'hidden'}`}
       >
-        {children}
+        {isOpen && children}
       </Content>
     </Wrapper>
   )
@@ -66,16 +110,17 @@ interface StyledProps {
   height: string;
   isInit: boolean;
   isOpen: boolean;
+  duration: number;
 }
 
-const Title = styled.div<Pick<StyledProps, 'isOpen'>>`
+export const Title = styled.div<Pick<StyledProps, 'isOpen' | 'duration'>>`
   display: flex;
   align-items: center;
   margin-bottom: 8px;
   cursor: pointer;
   &::before {
     margin-right: 8px;
-    transition: transform 300ms;
+    transition: transform ${({duration}) => duration}ms;
     ${({isOpen}) => !!isOpen && ({
       transform: 'rotate(90deg)'
     })}
@@ -85,20 +130,25 @@ const Title = styled.div<Pick<StyledProps, 'isOpen'>>`
     content: '▶'
   }
 `;
-const Content = styled.div<StyledProps>`
+export const Content = styled.div<Omit<StyledProps, 'isOpen'>>`
+  position: relative;
   padding-left: 8px;
   overflow: hidden;
-  ${({isInit, height, isOpen}) => `
+  left: 0; 
+  top: 0;
+  ${({isInit, height, duration}) => `
     ${isInit ? `
-      transition: height 300ms;
-      height: ${isOpen ? height : '0'};
+      transition: height ${duration}ms;
+      height: ${height};
     ` : `
+      position: absolute;
+      z-index: -9999;
       height: auto;
       visibility: hidden;
     `}
   `};
 `
 
-const Wrapper = styled.article`
+export const Wrapper = styled.article`
   position: relative;
 `
