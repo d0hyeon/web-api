49f09ecc278e76ba74ae042161b1d3462221f2dc
d0hyeon <ehgus6887@gmail.com>
2021-03-08 23:29:14 +0900

    feat: webpack dev server, socket server 결합

--- config.overrides.js
+++ config.overrides.js
@@ -1,21 +1,40 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = (config) => {
-  return {
-    ...config,
-    resolve: {
-      ...config.resolve,
-      modules: ['node_modules'],
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', 'css'],
-      alias: {
-        ...config.resolve.alias,
-        '@src': path.resolve(__dirname, 'src')
+module.exports = {
+  webpack: (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        modules: ['node_modules'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', 'css'],
+        alias: {
+          ...config.resolve.alias,
+          '@src': path.resolve(__dirname, 'src')
+        }
+      },
+      plugins: [
+        ...config.plugins,
+        new Dotenv()
+      ]
+    }
+  },
+  devServer: (configFunction) => {
+    return (_proxy = {}, allowedHost) => {
+      const proxy = {
+        ..._proxy,
+        '/socket.io': {
+          target: 'http://localhost:4000',
+          ws: true
+        }
       }
-    },
-    plugins: [
-      ...config.plugins,
-      new Dotenv()
-    ]
+      const config = configFunction(proxy, allowedHost);
+
+      return {
+        ...config,
+        setup: require('./socketServer')
+      }
+    }
   }
 }
