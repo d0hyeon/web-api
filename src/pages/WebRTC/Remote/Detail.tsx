4a20a56db71ff274f1aa7de05ce692a0728d6fd4
d0hyeon <ehgus6887@gmail.com>
2021-03-09 01:36:12 +0900

    feat: 상세 화면 접속 시 소켓에게 이벤트 송신

--- src/pages/WebRTC/Remote/Detail.tsx
+++ src/pages/WebRTC/Remote/Detail.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { io } from "socket.io-client";
 import { Header } from '@src/components/styles/common';
 import { H1 } from '@src/components/styles/text';
 
+const socket = io();
+
 const WebRTCRoomDetail = () => {
+  const { room } = useParams<{room: string}>();
+  
+  React.useEffect(() => {
+    socket.on('joinedRoom', () => {
+      console.log('join!');
+    })
+    socket.emit('joinRoom', room);
+    return () => {
+      socket.emit('leaveRoom', room);
+    }
+  }, []);
+
   return (
     <>
       <Header>
