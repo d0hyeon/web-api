3a01caff0fb2be3327cfa7d6de687eee14147d36
d0hyeon <ehgus6887@gmail.com>
2021-11-07 01:08:11 +0900

    refactor: highlight css > 모듈 내에서 가져오도록 변경

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@src/pages/App';
 
+import 'highlight.js/styles/monokai-sublime.css'
 
 ReactDOM.render(
   <React.StrictMode>
