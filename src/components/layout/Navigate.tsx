e0cc739c7adfd37d3e5d8974768b7a017876090e
d0hyeon <ehgus6887@gmail.com>
2021-11-07 01:16:18 +0900

    feat: 네비게이션 정렬 적용

--- src/components/layout/Navigate.tsx
+++ src/components/layout/Navigate.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { ROUTES } from '@src/constants/route';
 
 const Navigate: React.FC = () => {
+  const routes = useMemo(() => {
+    return ROUTES.sort((a, b) => {
+      if(a.title && b.title) {
+        return (a.title!.charCodeAt(0) < b.title!.charCodeAt(0)) ? -1 : 1
+      }
+      return 0;
+    })
+  }, [])
   return (
     <Wrapper className="navigate">
       <Title>목차</Title>
       <MenuList>
-        {ROUTES.map(({title, url}) => title && (
+        {routes.map(({title, url}) => title && (
           <li key={title}>
             <NavLink to={url} activeStyle={{color: '#C43326'}} exact>{title}</NavLink>
           </li>
